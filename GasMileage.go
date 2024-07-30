a21c7180f9d192f46ea67cb7d3050e276d9179a8
Ifeanyisunday <sundayjnr10@gmail.com>
2024-07-30 10:28:05 +0100

    go

---
+++ GasMileage.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var miles float64
+	var gallon float64
+
+	fmt.Println("Enter miles")
+	fmt.Scanln(&miles)
+	fmt.Println("Enter gallon")
+	fmt.Scanln(&gallon)
+
+	var result float64 = gallon / miles
+	fmt.Printf("%.2f", result)
+}
